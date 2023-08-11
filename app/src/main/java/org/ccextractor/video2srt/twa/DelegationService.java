b67eab9d168c4ca9be517e585d270f02b2c46e72
Matej Plavevski <matej.plavevski+github@gmail.com>
2023-08-11 14:09:43 +0200

    Bubblewrap files

---
+++ app/src/main/java/org/ccextractor/video2srt/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package org.ccextractor.video2srt.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
