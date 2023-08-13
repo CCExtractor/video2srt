382b3428f1e4974585fca8922174ec11f7c73f92
Matej Plavevski <matej.plavevski+github@gmail.com>
2023-08-13 17:27:59 +0200

    Update Viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,10 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
 
-import {config} from 'dotenv';
-import replace from '@rollup/plugin-replace';
-
-const configToReplace = {};
-for (const [key, v] of Object.entries(config().parsed)) {
-  configToReplace[`process.env.${key}`] = `'${v}'`;
-}
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     svelte(),
-    crossOriginIsolation(),
-    replace({
-      include: ["src/**/*.ts", "src/**/*.svelte"],
-      preventAssignment: true,
-      values: configToReplace
-    })
+    crossOriginIsolation()
   ],
 })
